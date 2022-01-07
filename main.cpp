29bbf7d23a28be7b3e9d5839a9d908c248511410
ygeslin <ygeslin@ygeslins-MacBook-Pro.local>
2022-01-07 10:58:06 +0100

    clean code debut parsing

--- main.cpp
+++ main.cpp
@@ -3,14 +3,8 @@
 #include "colors.h"
 #include "webserv.hpp"
 
-
-int main (int ac, char **av, char **env)
+void print_welcome( void )
 {
-	(void)ac;
-	(void)av;
-	(void)env;
-	webserv		server;
-
 	std::cout << BOLDCYAN;
 	std::cout << "oooooooooooooooooooooooooooooooooo" << std::endl;
 	std::cout << "O                                O" << std::endl;
@@ -19,31 +13,60 @@ int main (int ac, char **av, char **env)
 	std::cout << "oooooooooooooooooooooooooooooooooo" << std::endl;
 	std::cout << std::endl;
 	std::cout << RESET;
+	return ;
+}
+
+void usage_too_many_args ( void )
+{
+	std::cout << RED;
+	std::cerr << "To many arguments provided. 1 argument max." << std::endl ;
+	std::cerr << "Usage : ./webserv 'path/config_file'."<< std::endl;
+	std::cout << RESET;
+	exit (1);
+}
+
+void usage_one_arg ( void )
+{
+	std::cout << GREEN;
+	std::cout << "No argument provided." << std::endl;
+	std::cout << "Default configuration file will be used." << std::endl;
+	std::cout << "You can edit file at 'etc/webserv.conf'." << std::endl;
+	std::cout << RESET;
+	return ;
+}
+
+void usage_two_args ( char * str )
+{
+	std::cout << GREEN;
+	std::cout << "Loading '" << str << "'";
+	std::cout << " as configuration file."<< std::endl;
+	std::cout << RESET;
+	return ;
+}
+
+int main (int ac, char **av, char **env)
+{
+	(void)ac;
+	(void)av;
+	(void)env;
+	webserv		server;
+
+	print_welcome();
 
 	if (ac > 2)
-	{
-		std::cout << RED;
-		std::cerr << "To many arguments provided. 1 argument max." << std::endl ;
-		std::cerr << "Usage : ./webserv 'path/config_file'."<< std::endl;
-		std::cout << RESET;
-		return (1);
-	}
+		usage_too_many_args();
 	else
 	{
 		if (ac == 1)
 		{
-			std::cout << "No argument provided." << std::endl;
-			std::cout << "Default configuration file will be used." << std::endl;
-			std::cout << "You can edit file at 'etc/webserv.conf'." << std::endl;
+			usage_one_arg();
 			server.setFileName("etc/webserv.conf");
 		}
 		if (ac == 2)
 		{
-			std::cout << "Loading '" << av[1] << "'";
-			std::cout << " as configuration file."<< std::endl;
+			usage_two_args(av[1]);
 			server.setFileName(av[1]);
 		}
-
 	}
 
 	return (1);
