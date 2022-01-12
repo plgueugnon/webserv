63c0c5520d0cac7974a373562fc7b322bd0dfd53
Yann GESLIN <ygeslin@e1r12p16.42.fr>
2022-01-12 12:14:52 +0100

    add comments of directives

--- webserv.cpp
+++ webserv.cpp
@@ -16,41 +16,27 @@ std::string const & webserv::getFileName ( void ) const
 	return _file_name ;
 }
 
-
 /*
- * LIST OF CONTEXTS
- *	- MAIN
- *	- events
- *	- server
- *	- http
- *	- upstream
- *	- mail
+ * LIST OF CONTEXTS to implement
+ * - server
+ * - http
+ * - location
  *
- * LIST OF DIRECTIVES
- * -----------------------------------------------------------------------------
- * Syntax:	autoindex on | off;
- * -----------------------------------------------------------------------------
- * Default:	
- * autoindex off;
- * Context:	http, server, location
- * Enables or disables the directory listing output.
- * -----------------------------------------------------------------------------
- *  Syntax:	client_max_body_size size;
- * -----------------------------------------------------------------------------
- * Default:	client_max_body_size 1m;
- * Context:	http, server, location
- * Sets the maximum allowed size of the client request body. If the size in a
- * request exceeds the configured value, the 413 (Request Entity Too Large) error 
- * is returned to the client. Please be aware that browsers cannot correctly 
- * display this error. Setting size to 0 disables checking of client request body 
- * size.
- * -----------------------------------------------------------------------------
- * Syntax:	events { ... }
- * -----------------------------------------------------------------------------
- * Default:	—
- * Context:	main
- * Provides the configuration file context in which the directives that affect 
- * connection processing are specified.
+ * LIST OF DIRECTIVES to implement
+ * DIRECTIVE 				| CONTEXT
+ * --------------------------------------
+ * - http 					| N/A
+ * - server					| http
+ * - autoindex 				| http / server / location
+ * - client_max_body_size 	| http / server / location
+ * - index 				 	| http / server / location
+ * - error_page 		 	| http / server / location
+ * - root 		 			| http / server / location
+ * - location			 	| server / location
+ * - return				 	| server / location
+ * - listen 	 			| server
+ * - server_name 			| server
+ * - limit_except 			| location
  *
 
  */
