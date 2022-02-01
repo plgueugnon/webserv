f03f8bbfa57c09d11248325fdeb384a81d0ba2fb
P19 <pierre-louis@MacBook-Pro-de-P19.local>
2022-02-01 12:14:55 +0100

    preping to merge with main

--- listener.cpp
+++ listener.cpp
@@ -9,11 +9,15 @@
 // TODO Rajouter class/struct avec serveur, port et socket pour yann
 
 #define NUM_CLIENTS 512
+#define MAX_EVENTS 10
+#define REQUEST_TIMEOUT 30
+#define MONITOR_TIMEOUT_SEC 10
+#define MONITOR_TIMEOUT_NSEC 1000
 
 typedef struct s_set
 {
-	struct kevent rset;
-	struct kevent wset;
+	struct kevent	rset;
+	int				port;
 }	t_set;
 
 unsigned int	gettime(void)
@@ -37,7 +41,7 @@ struct client_data
 	long time;
 } clients[NUM_CLIENTS];
 
-int	get_client_socket(int fd) // va cherche le fd et renvoie son index
+int	get_client_socket(int fd) // * va cherche le fd et renvoie son index
 {
 	for (int i = 0; i < NUM_CLIENTS; i++)
 		if (clients[i].fd == fd)
@@ -50,10 +54,10 @@ int	add_client_socket(int fd)
 	int i;
 	if ( fd < 1)
 		return -1;
-	if ((i = get_client_socket(0)) == -1) // je cherche une place dispo dans struct clients
-		return -1; // plus de place disponible
-	clients[i].fd = fd; // stocke le socket client dans struct clients a l'index récupéré
-	clients[i].time = gettime();
+	if ((i = get_client_socket(0)) == -1) // * je cherche une place dispo dans struct clients
+		return -1; // * plus de place disponible
+	clients[i].fd = fd; // * stocke le socket client dans struct clients a l'index récupéré
+	clients[i].time = gettime() + (REQUEST_TIMEOUT * 1000); // ! chaque requete commence avec 30 sec (nginx = 60)
 	return 0;
 }
 
@@ -64,11 +68,11 @@ int	update_client_time(int fd)
 		return 0;
 	if ((i = get_client_socket(fd)) == -1)
 		return 0;
-	clients[i].time = gettime() + (30 * 1000);
+	clients[i].time = gettime() + (REQUEST_TIMEOUT * 1000);
 	return 1;
 }
 
-int	del_client_socket(int fd) // supprime le socket_client client, libère sa place et le close
+int	del_client_socket(int fd) // * supprime le socket_client client, libère sa place et le close
 {
 	int i;
 	if (fd < 1)
@@ -113,12 +117,12 @@ void	listener() // ! kqueue
 	ports.push_back(port3);
 // **************************************************
 	struct timespec	timeout;
-	timeout.tv_nsec = 1000;
-	timeout.tv_sec = 10;
+	timeout.tv_nsec = MONITOR_TIMEOUT_NSEC;
+	timeout.tv_sec = MONITOR_TIMEOUT_SEC;
 	char	late[] = "connection timeout !\n";
 // **************************************************
 	// ! je stocke les sockets d'écoute dans un tableau
-	std::vector<int> listen_sockets;
+	std::vector<int> listen_sockets; // TODO AJOUTER ici port dans t_set pour chaque listen socket
 	for( std::vector<int>::iterator it = ports.begin(); it != ports.end(); it++ )
 	{
 		try
@@ -133,34 +137,18 @@ void	listener() // ! kqueue
 	}
 // **************************************************
 
-	/* 
-	*	struct event dans laquelle on va stocker 
-	*    struct kevent {
-	*    uintptr_t	ident;	     identifier for this event = // ! fd, pid, signal etc
-	*    short     filter;	     filter for event = // ! EVFILT_READ or EVFILT_WRITE
-	*    u_short   flags;	     action flags for kqueue = // ! EV_ADD | EV_ENABLE pour ajouter // EV_DELETE pour supprimer
-	*    u_int     fflags;	     filter flag value
-	*    int64_t   data;	     filter data value
-	*    void      *udata;	     opaque user data identifier
-	*    uint64_t  ext[4];	     extensions
-    * };
-	*/
-	// struct kevent	evSet[3]; // events to monitor
-	std::vector<t_set>      evSet(3);
-	struct kevent	evList[10]; // events that will be triggered
+	std::vector<t_set>      evSet(3); // * events to monitor
+	struct kevent	evList[MAX_EVENTS]; // * events that will be triggered
 
 	// * init kqueue = Creates a new kernel event queue and returns a descriptor
 	int	kq = kqueue();
 
 	// * stocke socket serveur dans evSet en mode lecture = on crée un filtre d'évènement a surveiller pour chaque socket (si en lecture),
 	// * avec l'action correspondante (add + enable)
-	// for( std::vector<int>::iterator it = listen_sockets.begin(), int i = 0; it != listen_sockets.end(), i < 3; it++, i++ )
 	for (int i = 0; i < 3; i++)
 	{
 		EV_SET(&evSet[i].rset,listen_sockets[i], EVFILT_READ, EV_ADD | EV_ENABLE, 0, 0, 0);
-		// EV_SET(&evSet[i],listen_sockets[i], EVFILT_READ, EV_ADD | EV_ENABLE, 0, 0, 0);
 		// * on stocke les évènements qui vont être monitoré
-		// if (kevent(kq, &(evSet[i]), 1, NULL, 0, NULL) == -1)
 		if (kevent(kq, &(evSet[i].rset), 1, NULL, 0, NULL) == -1)
 		{
 			std::cerr << RED"error: kevent 1 failure\n"RESET;
@@ -168,23 +156,15 @@ void	listener() // ! kqueue
 		}
 	}
 
-	struct kevent	evCon; // struct pour stocker les events des sockets clients
+	struct kevent	evCon; // * struct pour stocker les events des sockets clients
 	int	client_sock;
 
+	// * Listening loop
 	while (1)
 	{
 		// * equivalent a select
-		/*
-		* arg 1 = fd event queue to monitor (en read ou write, il faut 1 queue par filtre specifique (fd read, fd write, signal, etc))
-		* arg 2 = event a surveiller
-		* arg 3 = n event a surveiller
-		* arg 4 = event a declencher
-		* arg 5 = n event a surveiller
-		* arg 6 = timeout
-		*/
 		std::cout << BLUE"Waiting for connection...\n"RESET;
 		int	num_events = kevent(kq, NULL, 0, evList, 10, &timeout);
-		// std::cout << "timer = " << num_events << std::endl;
 		if (num_events == 0)
 		{
 			unsigned int now = gettime();
@@ -196,18 +176,16 @@ void	listener() // ! kqueue
 					if (clients[i].time - now <= 0)
 					{
 						std::cout << GREEN"client #" << get_client_socket(clients[i].fd) << "with fd #" << clients[i].fd << " timeout\n"RESET;
-						// shutdown(clients[i].fd, SHUT_RDWR);
-						send(clients[i].fd, late, strlen(late), 0);
+
+						send(clients[i].fd, late, strlen(late), 0); // ! A modifier pour faire une reponse 408 timeout au client
 						close(clients[i].fd);
-						for (int j = 0; j < 10; j++)
+						for (int j = 0; j < MAX_EVENTS; j++)
 						{
 							if (evList[j].ident == (unsigned long)clients[i].fd)
 							{
 								std::cout << "entry test for client # " << i << " or " << j << std::endl;
-								// send(clients[i].fd, late, strlen(late), 0);
-								EV_SET(&evCon, evList[j].ident, EVFILT_READ, EV_DELETE, 0, 0, NULL);
+								EV_SET(&evCon, evList[j].ident, EVFILT_READ, EV_DELETE, 0, 0, NULL); // TODO RAjouter un kevent en write sur le meme fd
 								kevent(kq, &evCon, 1, NULL, 0, NULL); // actualise le fd set
-								// shutdown(evList[j].ident, SHUT_RDWR);
 								break ;
 							}
 						}
@@ -218,15 +196,15 @@ void	listener() // ! kqueue
 		}
 		for (int i = 0; i < num_events; i++)
 		{
-				// Si je trouve le même fd dans evList -> un client est prêt (en read) pour connexion
+				// * Si je trouve le même fd dans evList -> un client est prêt (en read) pour connexion
 				if (cycle_fd(listen_sockets, evList[i].ident))
 				{
 					// * on accepte la connexion : on crée un fd client
 					client_sock = accept(evList[i].ident, NULL, NULL);
-					if (add_client_socket(client_sock) == 0)
+					if (add_client_socket(client_sock) == 0) // ! NB A verifier : ajout de 1 seul socket mais avec 2 events
 					{
-						EV_SET(&evCon, client_sock, EVFILT_READ, EV_ADD | EV_ENABLE, 0, 0, NULL);
-						kevent(kq, &evCon, 1, NULL, 0, NULL);
+						EV_SET(&evCon, client_sock, EVFILT_READ, EV_ADD | EV_ENABLE, 0, 0, NULL); // TODO RAjouter un kevent en write sur le meme fd
+						kevent(kq, &evCon, 1, NULL, 0, NULL); // TODO augmenter a 2
 						send_welcome_msg(client_sock);
 					}
 					else
@@ -241,9 +219,9 @@ void	listener() // ! kqueue
 				else if (evList[i].flags & EV_EOF)
 				{
 					std::cout << GREEN"client #" << get_client_socket(evList[i].ident) << " disconnected\n";
-					EV_SET(&evCon, evList[i].ident, EVFILT_READ, EV_DELETE, 0, 0, NULL);
-					kevent(kq, &evCon, 1, NULL, 0, NULL); // actualise le fd set
-					del_client_socket(evList[i].ident);
+					EV_SET(&evCon, evList[i].ident, EVFILT_READ, EV_DELETE, 0, 0, NULL);  // TODO RAjouter un kevent en write sur le meme fd
+					kevent(kq, &evCon, 1, NULL, 0, NULL); // actualise le fd set  // TODO augmenter a 2
+					del_client_socket(evList[i].ident); 
 				}
 				else if (evList[i].filter == EVFILT_READ)
 				{
@@ -251,8 +229,8 @@ void	listener() // ! kqueue
 					std::cout << "client #" << r << " old time = " << clients[r].time << "\n";
 					if (!receive_request(evList[i].ident))
 					{
-						EV_SET(&evCon, evList[i].ident, EVFILT_READ, EV_DELETE, 0, 0, NULL);
-						kevent(kq, &evCon, 1, NULL, 0, NULL); // actualise le fd set
+						EV_SET(&evCon, evList[i].ident, EVFILT_READ, EV_DELETE, 0, 0, NULL);  // TODO RAjouter un kevent en write sur le meme fd
+						kevent(kq, &evCon, 1, NULL, 0, NULL); // actualise le fd set  // TODO augmenter a 2
 						del_client_socket(evList[i].ident);
 					}
 					else
@@ -262,99 +240,9 @@ void	listener() // ! kqueue
 						// std::cout << "client #" << j << " updated time = " << clients[j].time << " with fd #" << clients[j].fd << "\n";
 					}
 				}
-				// if (num_events == 0)
-				// {
-				// 	std::cout << GREEN"client #" << get_client_socket(evList[i].ident) << " timeout\n";
-				// 	send(get_client_socket(evList[i].ident), late, strlen(late), 0);
-				// 	EV_SET(&evCon, evList[i].ident, EVFILT_READ, EV_DELETE, 0, 0, NULL);
-				// 	kevent(kq, &evCon, 1, NULL, 0, NULL); // actualise le fd set
-				// 	del_client_socket(evList[i].ident);
-				// }
 		}
 
-	}
+	} // TODO Ajouter boucle de fermeture des sockets clients puis serveur en cas de signal
+		// TODO tester avec shutdown(clients[i].fd, SHUT_RDWR);
 
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-// ! Faire passer ici en arg les spec de configs
-void	listener_select()
-{
-	int port1 = 18000;
-	int port2 = 8200;
-	int port3 = 8080;
-
-	std::vector<int> ports;
-	ports.push_back(port1);
-	ports.push_back(port2);
-	ports.push_back(port3);
-
-	// ! fd_set de lecture -> pour déterminer si le socket server est prêt pour lecture
-	fd_set	read_fs;
-	// ! obligatoire d'init la struct avec FD_ZERO pour pouvoir y stocker des sockets
-	FD_ZERO(&read_fs);
-	// fd_set	write_fs;
-	int	client_sock;
-
-	fd_set	reader = { 0 };
-
-	// ! je stocke les sockets d'écoute dans un tableau
-	std::vector<int> listen_sockets;
-	for( std::vector<int>::iterator it = ports.begin(); it != ports.end(); it++ )
-	{
-		try
-		{
-			listen_sockets.push_back(gen_listen_socket(*it));
-		}
-		catch ( const std::exception &e )
-		{
-			std::cerr << e.what();
-			return ;
-		}
-	}
-
-	// ! puis je stocke les sockets dans une struct read_fs
-	for( std::vector<int>::iterator it = listen_sockets.begin(); it != listen_sockets.end(); it++ )
-		FD_SET(*it, &read_fs);
-
-	// * Listening loop
-	while (true)
-	{
-		// ! necessaire de réactualiser le reader_fd a chaque tour pour acceder à un nouveau port
-		reader = read_fs;
-		if (VERBOSE)
-			std::cout << BLUE"Waiting for connection...\n"RESET;
-		if (select(FD_SETSIZE, &reader, NULL, NULL, NULL) < 0)
-		{
-			std::cerr << RED"error: select failure : " << errno << "\n"RESET;
-			return ;
-		}
-
-		for(std::vector<int>::iterator it = listen_sockets.begin(); it != listen_sockets.end(); it++ )
-			if ( FD_ISSET(*it, &reader) )
-			{
-				if ((client_sock = accept(*it, NULL, NULL)) == -1)
-				{
-					std::cerr << RED"error: fail to accept connection request\n"RESET;
-					return ;
-				}
-				if (VERBOSE)
-					std::cout << GREEN"New client connection...\n"RESET;
-			}
-
-		receive_request(client_sock);
-	}
-	// TODO Ajouter boucle de fermeture des sockets serveur
-	// ? Capture du signal pour terminer programme ? Doit être géré ?
 }
