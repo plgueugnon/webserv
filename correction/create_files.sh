15d12571a3409a6b919ae61236a75e2c07fdba2f
Yann GESLIN <ygeslin@e1r10p19.42.fr>
2022-02-14 14:02:46 +0100

    create correction folder

--- correction/create_files.sh
+++ correction/create_files.sh
@@ -6,8 +6,6 @@ echo "kikoo je suis un bulbizare" > www/pokedex_to_delete/bulbizare
 echo "kikoo je suis un salameche" > www/pokedex_to_delete/salameche
 echo "kikoo je suis un mewtow" > www/pokedex_to_delete/mewtow
 
-chmod 755 www/pokedex_to_delete/mewtow
-
 # curl -X DELETE http://localhost:8080/pokedex_to_delete/bulbizare
 # curl -X DELETE http://localhost:8080/pokedex_to_delete/mewtow
 # curl -X DELETE http://localhost:8080/pokedex_to_delete/salameche
