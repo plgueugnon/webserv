9081a2c0e080f08d8aa3270198d89b34bb52f8c0
Yann GESLIN <ygeslin@e1r10p19.42.fr>
2022-02-15 16:34:51 +0100

    add download.sh

--- correction/download.sh
+++ correction/download.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 
-curl -X DELETE http://localhost:8080/nodelete/index.html
-curl -X POST http://localhost:8080/nopost/index.html
-curl -X GET http://localhost:8080/noget/index.html
+curl -X GET http://localhost:8080/pikachu/index.html -o index.html
+curl -X GET http://localhost:8080/pokemon/carapuce -o carapuce
