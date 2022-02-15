6b2f098cd567b3563eaf2f4cfa551a77c91b2387
Yann GESLIN <ygeslin@e1r10p19.42.fr>
2022-02-15 16:04:37 +0100

    change upload dir to check extension

--- www/upload/upload_script.php
+++ www/upload/upload_script.php
@@ -1,13 +1,27 @@
 <?php
 // Sauvegarde de tous les fichiers passés en input
 foreach ($_FILES as $file) {
+    // var_dump($file);
+    $errors= array();
+    $path = $file['name'];
+    $ext = pathinfo($path, PATHINFO_EXTENSION);
+    $extensions= array("html", "txt", "php");
+    // var_dump($ext);
+    if(in_array($ext,$extensions)=== false)
+    {
+        $errors[]="extension not allowed";
+     }
+
     $uploadfile = $_ENV['TMP_DIR'] . basename($file['name']);
-    echo "uploading => " . $file['name'] . "\n";
-    if (move_uploaded_file($file['tmp_name'], $uploadfile)){
+    if (empty($errors)==true)
+    {
+        echo "uploading => " . $file['name'] . "\n";
+        (move_uploaded_file($file['tmp_name'], $uploadfile));
         echo "Le fichier " . $file['name'] . " a bien été envoyé\n";
-    } else {
+    } 
+    else 
+    {
+        print_r($errors);
         echo "Erreur dans l'envoi du fichier " . $file['name'] . "\n";
     }
 }
-
-?>
