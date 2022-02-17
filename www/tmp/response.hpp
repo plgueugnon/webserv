9620b16d994d64b0bf38ffdae3adbb4253725a7d
Yann GESLIN <ygeslin@e1r10p19.42.fr>
2022-02-17 16:37:28 +0100

    le upload script est pimpee pour afficher du hatml ush

---
+++ www/tmp/response.hpp
@@ -0,0 +1,78 @@
+#ifndef RESPONSE_HPP
+#define RESPONSE_HPP
+
+#define R_BUFFER_SIZE 10000
+
+class response
+{
+	public :
+
+	// conf
+	request 		req;
+	t_server 		conf;
+	t_location 		loc;
+
+	// response
+	int				code;
+	std::fstream	file;
+	// std::string 	fileName;
+	std::string 	root;
+	std::string 	path;
+	std::string 	index;
+	std::string 	buffer;
+	std::string 	output;
+	std::string 	ret;
+
+	int				read_fd[2];
+	int				write_fd[2];
+	// pid_t	pid;
+	// char	*argv[3];
+	char	read_buf[R_BUFFER_SIZE];
+	// class cgi cgi;
+
+
+
+	// iterator
+	std::vector<std::string>::iterator 	it;
+	std::vector<t_location>::iterator	loc_it;
+	// std::vector<std::string>::iterator end;
+
+	// constructors
+	response (void);
+	response (request request, t_server config);
+
+	void setCGIfd (int client_read_fd[2], int client_write_fd[2]);
+	void closeAllfd ( void );
+
+	void setRoot ( void );
+	void setPath ( void );
+	void setIndex ( void );
+	void setFile ( void );
+	void setLocation ( void );
+	void setCode ( int code);
+
+	void parse ( void );
+	void handleGet ( void );
+	void handleDelete ( void );
+	void handlePost (  void );
+
+	void exec_child( pid_t pid, cgi *cgi );
+	void write_to_cgi( void );
+	void read_from_cgi( void );
+	void RemoveLine(std::string& source, const std::string& to_remove);
+
+	std::string getAutoIndex ( std::string fileName );
+	std::string getErrorPage (std::vector<std::string> vec );
+	std::string getDataFromFile(std::string fileName);
+
+	void redirectRequest (std::vector<std::string> *vec);
+
+	bool isMethodAllowed (std::string method);
+	bool isMethodImplemented ( void );
+	bool isRedirected (std::vector<std::string> *vec);
+	bool isBodyTooLarge ( void );
+	bool isChunked ( void );
+	bool isURITooLong(void);
+};
+
+#endif /* RESPONSE_HPP */
--- www/upload/upload_script.php
+++ www/upload/upload_script.php
@@ -1,12 +1,12 @@
 <?php
 // Sauvegarde de tous les fichiers passés en input
 foreach ($_FILES as $file) {
-    var_dump($file);
+    // var_dump($file);
     $errors= array();
     $path = $file['name'];
     $ext = pathinfo($path, PATHINFO_EXTENSION);
-    $extensions= array("html", "txt", "php", "cpp", "hpp");
-    var_dump($ext);
+    $extensions= array("html", "txt", "php", "hpp");
+    // var_dump($ext);
     if(in_array($ext,$extensions)=== false)
     {
         $errors[]="extension not allowed";
@@ -15,13 +15,17 @@ foreach ($_FILES as $file) {
     $uploadfile = $_ENV['TMP_DIR'] . basename($file['name']);
     if (empty($errors)==true)
     {
-        echo "uploading => " . $file['name'] . "\n";
+        echo "<!DOCTYPE html> <html> <body><h1><h1>";
+        // echo "uploading => " . $file['name'] . "\n";
         (move_uploaded_file($file['tmp_name'], $uploadfile));
-        echo "Le fichier " . $file['name'] . " a bien ete envoye\n";
+        echo "File '" . $file['name'] . "' was successfuly uploaded\n";
+        echo " </h1> </body> </html>";
     } 
     else 
     {
-        print_r($errors);
-        echo "Erreur dans l'envoi du fichier " . $file['name'] . "\n";
+        echo "<!DOCTYPE html> <html> <body><h1><h1 style=\"color:red;\">";
+        echo "Error : Extension is not allowed !";
+        echo " </h1> </body> </html>";
+        // echo "Erreur dans l'envoi du fichier " . $file['name'] . "\n";
     }
 }
